9cfcdec94b3a413a326db3ace0fff780cda0f96a
MeizuPro <meizumxpro0@gmail.com>
2021-04-25 21:08:45 +0300

    Init game

---
+++ alien_invasion.py
@@ -0,0 +1,36 @@
+import sys
+import pygame
+
+class AlienInvasion:
+    """"Управление ресурами игры."""
+
+    def __init__(self):
+        """"Инициализация игры."""
+        pygame.init()
+
+        self.screen = pygame.display.set_mode((1200, 800))
+        pygame.display.set_caption("Alien Invasion")
+        self.bg_color = (230,230,230)
+    
+    def run_game(self):
+        """"Запуск нового цикла игры."""
+        while True:
+            #Отслеживаем события клавиатуры и мыши
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    sys.exit()
+
+            self.screen.fill(self.bg_color)
+
+            #Отображение последнего прорисованного экрана
+            pygame.display.flip()
+
+if __name__ == '__main__':
+    #Создание экземпляра и запуск игры.
+    ai = AlienInvasion()
+    ai.run_game()
+
+
+
+
+
