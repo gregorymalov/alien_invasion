c5304049b26a7d287ea3dc5ba6f70704f446ee21
MeizuPro <meizumxpro0@gmail.com>
2021-04-26 00:26:19 +0300

    The ship can move left and right

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,30 +1,54 @@
 import sys
 import pygame
 from settings import Settings
+from ship import Ship
 
 class AlienInvasion:
-    """"Управление ресурами игры."""
+    """Управление ресурами игры."""
 
     def __init__(self):
-        """"Инициализация игры."""
+        """Инициализация игры."""
         pygame.init()
         self.settings = Settings()
 
         self.screen = pygame.display.set_mode(
             (self.settings.screen_width, self.settings.screen_height))
+
         pygame.display.set_caption("Alien Invasion")
+
+        self.ship = Ship(self)
     
     def run_game(self):
-        """"Запуск нового цикла игры."""
+        """Запуск нового цикла игры."""
         while True:
-            #Отслеживаем события клавиатуры и мыши.
+            self._check_events()
+            self.ship.update()
+            self._update_screen()
+
+    def _check_events(self):
+            """Отслеживаем события клавиатуры и мыши."""
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     sys.exit()
-            #Меняем фон
-            self.screen.fill(self.settings.bg_color)
 
-            #Отображение последнего прорисованного экрана.
+                    #перемещаемся вправо/влево
+                elif event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_RIGHT:
+                        self.ship.moving_right = True
+                    elif event.key == pygame.K_LEFT:
+                        self.ship.moving_left = True
+                elif event.type == pygame.KEYUP:
+                    if event.key == pygame.K_RIGHT:
+                        self.ship.moving_right = False
+                    elif event.key == pygame.K_LEFT:
+                        self.ship.moving_left = False
+                        
+
+
+    def _update_screen(self):
+            """Обновляет изображение на экране и отображает новый экран"""
+            self.screen.fill(self.settings.bg_color)
+            self.ship.blitme()
             pygame.display.flip()
 
 if __name__ == '__main__':
---
+++ ship.py
@@ -0,0 +1,30 @@
+import pygame
+
+class Ship():
+    """Модуль коробля"""
+    def __init__(self, ai_game):
+        """Инициализируем корабль и задаём начальную позицию"""
+        self.screen = ai_game.screen
+        self.screen_rect = ai_game.screen.get_rect()
+
+        #Загружаем картинку
+        self.image = pygame.image.load('images/ship.bmp')
+        self.rect = self.image.get_rect()
+        #Каждый новый корабль появляется у нижнего края экрана
+        self.rect.midbottom = self.screen_rect.midbottom
+
+        #Флаг перемещения
+        self.moving_right = False
+        self.moving_left = False
+    
+    def update(self):
+        """Обновляет позицию корабля с учетом флага"""
+        if self.moving_right:
+            self.rect.x += 1
+        if self.moving_left:
+            self.rect.x -= 1
+    
+
+    def blitme(self):
+        """Рисуем корабль в текущей позиции"""
+        self.screen.blit(self.image, self.rect)
