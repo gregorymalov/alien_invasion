113886a233aea595db78813efe937b21aabf0ae5
MeizuPro <meizumxpro0@gmail.com>
2021-04-27 15:20:44 +0300

    Add settings speed and travel restrictions

--- settings.py
+++ settings.py
@@ -7,3 +7,6 @@ class Settings():
         self.screen_width = 1200
         self.screen_height = 1000
         self.bg_color = (0, 0, 0)
+        
+        #Настройка скорости коробля
+        self.ship_speed = 1.5
--- ship.py
+++ ship.py
@@ -5,13 +5,18 @@ class Ship():
     def __init__(self, ai_game):
         """Инициализируем корабль и задаём начальную позицию"""
         self.screen = ai_game.screen
+        self.settings = ai_game.settings
         self.screen_rect = ai_game.screen.get_rect()
 
         #Загружаем картинку
         self.image = pygame.image.load('images/ship.gif')
         self.rect = self.image.get_rect()
+
         #Каждый новый корабль появляется у нижнего края экрана
         self.rect.midbottom = self.screen_rect.midbottom
+
+        #Сохранение вещественной координаты центра коробля.
+        self.x = float(self.rect.x)
 
         #Флаг перемещения
         self.moving_right = False
@@ -19,10 +24,14 @@ class Ship():
     
     def update(self):
         """Обновляет позицию корабля с учетом флага"""
-        if self.moving_right:
-            self.rect.x += 1
-        if self.moving_left:
-            self.rect.x -= 1
+        #Обновляем атрибут x, не rect
+        if self.moving_right and self.rect.right  <  self.screen_rect.right:
+            self.x += self.settings.ship_speed
+        if self.moving_left and self.rect.left  >  0:
+            self.x -= self.settings.ship_speed
+
+        #Обновление атрибута rect на основании self.x
+        self.rect.x = self.x
     
 
     def blitme(self):
